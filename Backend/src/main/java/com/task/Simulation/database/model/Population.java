3e380dc39689a299b0f8a02b28f82ce13d523cde
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-07-29 18:24:55 +0200

    Changed directory structure

---
+++ Backend/src/main/java/com/task/Simulation/database/model/Population.java
@@ -0,0 +1,13 @@
+package com.task.Simulation.database.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Population {
+}
